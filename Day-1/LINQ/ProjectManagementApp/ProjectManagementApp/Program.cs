a9a9adc1ce5e6c6701c12efa29da6fdb84bd8a98
Magesh <tkmagesh77@gmail.com>
2013-06-13 09:39:42 +0530

    Lambda Expressions

--- Day-1/LINQ/ProjectManagementApp/ProjectManagementApp/Program.cs
+++ Day-1/LINQ/ProjectManagementApp/ProjectManagementApp/Program.cs
@@ -17,16 +17,60 @@ namespace ProjectManagementApp
             products.Add(new Product { Id = 45, Name = "Den", Cost = 34, Units = 31, Category = 1 });
             products.Add(new Product { Id = 13, Name = "Hen", Cost = 22, Units = 15, Category = 2 });
 
-            printProducts("Initial List", products);
+            PrintProducts("Initial List", products);
+            var categoryOneProducts = products.Search(new ProductSearchCriteriaByCategory(1));
+            PrintProducts("Category - 1 Products",categoryOneProducts);
+            //var categoryTwoProducts = products.Search(new ProductCriteriaDelegate(IsProductBelongToCategoryTwo));
+            //var categoryTwoProducts = products.Search(IsProductBelongToCategoryTwo);
+            /*var categoryTwoProducts = products.Search(delegate(Product product)
+                {
+                    return product.Category == 2;
+                });
+            */
+            /*var categoryTwoProducts = products.Search((product) =>
+                {
+                    return product.Category == 2;
+                });*/
+            var categoryTwoProducts = products.Search(product => product.Category == 2);
+            PrintProducts("Category - 2 Products", categoryTwoProducts);
+            
             Console.ReadLine();
+
         }
 
-        private static void printProducts(string title, ProductsCollection products)
+        public static bool IsProductBelongToCategoryTwo(Product product)
+        {
+            return product.Category == 2;
+        }
+
+        private static void PrintProducts(string title, ProductsCollection products)
         {
             Console.WriteLine(title);
             Console.WriteLine("===========================================");
-            for(var i=0;i<products.Count;i++)
-                Console.WriteLine(products.GetByIndex(i));
+            /*for(var i=0;i<products.Count;i++)
+                Console.WriteLine(products[i]);*/
+            foreach (var product in products)
+            {
+                Console.WriteLine(product);
+            }
+            Console.WriteLine();
         }
     }
+
+    public class ProductSearchCriteriaByCategory : IProductSearchCriteria
+    {
+        private readonly int _category;
+
+        public ProductSearchCriteriaByCategory(int category)
+        {
+            _category = category;
+        }
+
+        public bool IsSatisfiedBy(Product product)
+        {
+            return product.Category == _category;
+        }
+    }
+
+    public delegate bool ProductCriteriaDelegate(Product product);
 }
